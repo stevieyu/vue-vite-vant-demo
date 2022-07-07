654cb95c24f92e9d80c71fb09ece745e44e3b57e
StevieYu <stevieyu@users.noreply.github.com>
2022-07-07 10:01:18 +0800

    Update RemoteLibraries.js

--- src/helpers/RemoteLibraries.js
+++ src/helpers/RemoteLibraries.js
@@ -67,29 +67,6 @@ export const clipboard = async(...args) => {
     })
 }
 
-export const setTitle = (title, img) => {
-    if (title === undefined || window.document.title === title) {
-      return
-    }
-    document.title = title
-    var mobile = navigator.userAgent.toLowerCase()
-    if (/iphone|ipad|ipod/.test(mobile)) {
-      const iframe = document.createElement('iframe')
-      iframe.style.display = 'none'
-      // 替换成站标favicon路径或者任意存在的较小的图片即可，支付宝小程序引用默认空白的base64图片会有安全警告
-      const _img = /alipay/.test(mobile) ? img : (img || 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
-      _img && iframe.setAttribute('src', _img)
-      const iframeCallback = function () {
-        setTimeout(() => {
-          iframe.removeEventListener('load', iframeCallback)
-          document.body.removeChild(iframe)
-        }, 0)
-      }
-      iframe.addEventListener('load', iframeCallback)
-      document.body.appendChild(iframe)
-    }
-  }
-
 
 globalThis.$t = {
     QRCodeToDataURL,
