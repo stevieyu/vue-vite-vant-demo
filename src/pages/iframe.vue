4994545f8c3f5d2fbebbe836e2674cac7cb820dd
StevieYu <yzm629@sina.cn>
2022-07-20 09:50:24 +0000

    feat: iframe src fot query

--- src/pages/iframe.vue
+++ src/pages/iframe.vue
@@ -1,6 +1,11 @@
 <template>
   <iframe
-    src="http://vue-wechat-title.deboy.cn"
+    :src="route.query.s || `http://vue-wechat-title.deboy.cn`"
     class="border-0 w-full h-screen"
   />
 </template>
+<script setup>
+import {useRoute} from 'vue-router';
+
+const route = useRoute();
+</script>
